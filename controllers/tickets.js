34d2f0f5aaaadb6c6c3c1186818eb3c78a8c9092
tribaaldos <tribaaldos@gmail.com>
2023-05-26 01:05:04 -0700

    part3

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,23 +1,18 @@
-const Ticket = require('../models/ticket');
-const Flight = require('../models/flight');
+const Ticket = require('../models/ticket')
 
 module.exports = {
     new: newTicket,
     create,
-};
-
+}
+function newTicket (req, res) {
+    res.render('tickets/new', { flightId: req.params.id })
+}
 async function create(req, res) {
     try{
         req.body.flight = req.params.id
-        await Ticket.create(req.body)
-        console.log(req.body)
-        res.redirect(`/flights/${req.params.id}`)
+       await Ticket.create(req.body);
+       res.redirect(`/flights/${req.params.id}`)
     } catch (err) {
         console.log(err)
     }
-}
-
-async function newTicket(req, res) {
-    let flightId = req.params.id;
-    res.render('tickets/new', {flightId})
 }
